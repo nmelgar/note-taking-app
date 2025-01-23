99bf7f47e4a7ddbb159b3ddeb903110d1f8fa1d7
nmelgar <nmelgar16013@gmail.com>
2025-01-23 17:49:06 -0600

    Update code to connect to db. Update uri and credentials.

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-const { MongoClient } = require("mongodb")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = require("./atlas_uri")
-
-console.log(uri)
-
-const cliente = new MongoClient(uri)
-const dbname = "bank"
-
-const connectToDatabase = async () => {
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
     try {
-        await cliente.connect();
-        console.log(`Connected to the ${dbname} database`);
-    }
-    catch (err) {
-        console.error(`Error connecting to the database: ${err}`);
-    }
-};
-
-const main = async () => {
-    try {
-        await connectToDatabase();
-    }
-    catch (err) {
-        console.error(`Error connecting to the database: ${err}`);
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
+        // Ensures that the client will close when you finish/error
         await client.close();
     }
-};
-main();
+}
+run().catch(console.dir);
